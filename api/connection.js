cb7137ee0583c18dc82d120fde2d7e0deb971fbd
lanstheprodigy <alan.040219.gam@gmail.com>
2025-12-29 10:02:35 +0700

    fix connection db

--- api/connection.js
+++ api/connection.js
@@ -3,17 +3,31 @@ require("dotenv").config();
 const ENV = process.env;
 
 //koneksi database
-const conn = mysql.createConnection({
-  host: ENV.DB_HOST || "localhost",
+const conn = mysql.createPool({
+  host: ENV.DB_HOST || "mysql",
   user: ENV.DB_USER || "root",
   password: ENV.DB_PASSWORD || "",
   database: ENV.DB_NAME || "concert_hub",
   port: ENV.DB_PORT || 3306,
+
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
-conn.connect((err) => {
-  if (err) throw err;
-  console.log("MySQL Connected");
+conn.getConnection((err, connection) => {
+  if (err) {
+    console.error("MySQL pool connection error:", err.code, err.message);
+    return;
+  }
+  console.log("MySQL Pool Connected");
+  connection.release();
 });
+
+
+// conn.connect((err) => {
+//   if (err) throw err;
+//   console.log("MySQL Connected");
+// });
 
 module.exports = conn;
